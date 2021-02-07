37688eeb7e4eb12bc0a8ee44d78307359ccb860a
Straight <gengzhi1993@163.com>
2021-02-07 13:59:57 +0800

    修复只有文字可以点击的问题

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,25 +3,26 @@ import {Link} from 'react-router-dom';
 import React from 'react';
 import Icon from './Icon';
 
-
 const NavWrapper = styled.div`
   line-height: 24px;  
   box-shadow: 0 0 3px rgba(0,0,0,0.25);
   >ul{
     display: flex;
     >li{
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      width: 33.33333%;
       text-align: center;
-      padding: 4px 0;
-      >svg{
-        width: 24px;
-        height: 24px;
+      width: 33.33333%;
+      >a{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        padding: 4px 0;
+        >svg{
+          width: 24px;
+          height: 24px;
+        }
       }
-     }
+    }
   }
 `;
 const Nav = () => {
@@ -29,16 +30,22 @@ const Nav = () => {
     <NavWrapper>
       <ul>
         <li>
-          <Icon name = 'tag'/>
-          <Link to = "/tags">标签页面</Link>
+          <Link to = "/tags">
+            <Icon name = 'tag'/>
+            标签页面
+          </Link>
         </li>
         <li>
-          <Icon name = 'money'/>
-          <Link to = "/money">记账页面</Link>
+          <Link to = "/money">
+            <Icon name = 'money'/>
+            记账页面
+          </Link>
         </li>
         <li>
-          <Icon name = 'chart'/>
-          <Link to = "/statistics">统计页面</Link>
+          <Link to = "/statistics">
+            <Icon name = 'chart'/>
+            统计页面
+          </Link>
         </li>
       </ul>
     </NavWrapper>);
