5cd7630a357b929be701cb1393fc28f415a50f20
Straight <gengzhi1993@163.com>
2021-02-07 20:34:44 +0800

    NumberPad 样式

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -57,7 +57,60 @@ const CategorySection = styled.section`
     }
   }
 `;
-const NumberPad = styled.section``;
+const NumberPad = styled.section`
+  display: flex;
+  flex-direction: column;
+  > output{
+    background: white;
+    font-size: 36px;
+    line-height: 72px;
+    text-align: right;
+    padding: 0 16px;
+    box-shadow: inset 0 -5px 5px -5px rgba(0,0,0,0.25),
+                inset 0 5px 5px -5px rgba(0,0,0,0.25);  }
+  > div{
+    float:left;flex-wrap: wrap;
+    > button{
+      border: none;
+      font-size: 18px;
+      width: 20%;height: 64px;
+      &.zero{
+        width: 40%;
+      }
+      &.ok{
+        height: 128px;
+        float: right;
+      }
+      &:nth-child(1){
+        background: #f2f2f2;
+      }
+      &:nth-child(2),&:nth-child(6){
+        background: #e0e0e0;
+      }
+      &:nth-child(3),&:nth-child(7),
+      &:nth-child(11){
+        background: #d3d3d3;
+      }
+      &:nth-child(4),&:nth-child(8),
+      &:nth-child(12),&:nth-child(16){
+        background: #c1c1c1;
+      }
+      &:nth-child(5),&:nth-child(9),
+      &:nth-child(13){
+        background: #d8d8d8;
+      }
+      &:nth-child(10),&:nth-child(14),&:nth-child(17){
+        background: #a9a9a9;
+      }
+      &:nth-child(18){
+        background:#9a9a9a;
+      }
+      &:nth-child(15){
+      background: #8b8b8b;
+      }
+    }
+  }
+`;
 
 function Money() {
   return (
@@ -74,17 +127,18 @@ function Money() {
       <NotesSection>
         <label>
           <span>备注</span>
-          <input type = "text" placeholder='在这里输入备注'/>
+          <input type = "text" placeholder = '在这里输入备注'/>
         </label>
       </NotesSection>
       <CategorySection>
         <ul>
-          <li className='selected'>支出</li>
+          <li className = 'selected'>支出</li>
           <li>收入</li>
         </ul>
       </CategorySection>
       <NumberPad>
-        <div>100</div>
+        <output>100</output>
+        <div className='clearfix'>
         <button>1</button>
         <button>2</button>
         <button>3</button>
@@ -98,11 +152,12 @@ function Money() {
         <button>7</button>
         <button>8</button>
         <button>9</button>
-        <button>0</button>
-        <button>.</button>
         <button>*</button>
+        <button className='ok'>ok</button>
+        <button>.</button>
+        <button className='zero'>0</button>
         <button>÷</button>
-        <button>ok</button>
+        </div>
       </NumberPad>
     </Layout>
   );
