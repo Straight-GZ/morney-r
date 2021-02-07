6c8b70ff18de95b22892dc9e465daeddf34396c6
Straight <gengzhi1993@163.com>
2021-02-08 00:33:07 +0800

    NumberPadSection work in progress

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -19,29 +19,7 @@ function Money() {
       <TagsSection/>
       <NoteSection/>
       <CategorySection/>
-      <NumberPad>
-        <output>100</output>
-        <div className='clearfix'>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>+</button>
-        <button>删除</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>-</button>
-        <button>清空</button>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button>*</button>
-        <button className='ok'>ok</button>
-        <button>.</button>
-        <button className='zero'>0</button>
-        <button>÷</button>
-        </div>
-      </NumberPad>
+      <NumberPad/>
     </NewLayout>
   );
 }
