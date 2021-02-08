67d14aa5279fbdc18ffebb145fcec88d477cd1dd
Straight <gengzhi1993@163.com>
2021-02-08 10:09:45 +0800

    数字面板基本完成

--- src/views/Money/NumberPadSection.tsx
+++ src/views/Money/NumberPadSection.tsx
@@ -56,7 +56,15 @@ const Wrapper = styled.section`
   }
 `;
 const NumberPad = () => {
-  const [output, setOutput] = useState('0');
+  const [output, _setOutput] = useState('0');
+  const setOutput = (output: string) => {
+    if (output.length > 16) {
+      output = output.slice(0, 16);
+    } else if (output.length === 0) {
+      output = '0';
+    }
+    _setOutput(output);
+  };
   const onClickButtonWrapper = (e: React.MouseEvent) => {
     const text = (e.target as HTMLButtonElement).textContent;
     if (!text) {return;}
@@ -84,13 +92,18 @@ const NumberPad = () => {
         console.log(text);
         break;
       case '.':
-        console.log('点');
+        if (output.indexOf('.') >= 0) {return;}
+        setOutput(output + '.');
         break;
       case '清空':
-        console.log('清空');
+        setOutput('');
         break;
       case '删除':
-        console.log('删除');
+        if (output.length === 1) {
+          setOutput('');
+        } else {
+          setOutput(output.slice(0, -1));
+        }
         break;
       case 'ok':
         console.log('ok');
