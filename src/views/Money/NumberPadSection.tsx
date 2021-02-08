b28da06adda303ec1779865e902627f117f82775
Straight <gengzhi1993@163.com>
2021-02-08 11:44:30 +0800

    删除加减乘除

--- src/views/Money/NumberPadSection.tsx
+++ src/views/Money/NumberPadSection.tsx
@@ -17,9 +17,9 @@ const Wrapper = styled.section`
     > button{
       border: none;
       font-size: 18px;
-      width: 20%;height: 64px;
+      width: 25%;height: 64px;
       &.zero{
-        width: 40%;
+        width: 50%;
       }
       &.ok{
         height: 128px;
@@ -28,29 +28,25 @@ const Wrapper = styled.section`
       &:nth-child(1){
         background: #f2f2f2;
       }
-      &:nth-child(2),&:nth-child(6){
+      &:nth-child(2),&:nth-child(5){
         background: #e0e0e0;
       }
-      &:nth-child(3),&:nth-child(7),
-      &:nth-child(11){
+      &:nth-child(3),&:nth-child(6),
+      &:nth-child(9){
         background: #d3d3d3;
       }
-      &:nth-child(4),&:nth-child(8),
-      &:nth-child(12),&:nth-child(16){
+      &:nth-child(4),&:nth-child(7),
+      &:nth-child(10),&:nth-child(13){
         background: #c1c1c1;
       }
-      &:nth-child(5),&:nth-child(9),
-      &:nth-child(13){
+      &:nth-child(8),&:nth-child(11){
         background: #d8d8d8;
       }
-      &:nth-child(10),&:nth-child(14),&:nth-child(17){
+      &:nth-child(14){
         background: #a9a9a9;
       }
-      &:nth-child(18){
+      &:nth-child(12){
         background:#9a9a9a;
-      }
-      &:nth-child(15){
-      background: #8b8b8b;
       }
     }
   }
@@ -117,21 +113,17 @@ const NumberPad = () => {
         <button>1</button>
         <button>2</button>
         <button>3</button>
-        <button>+</button>
         <button>删除</button>
         <button>4</button>
         <button>5</button>
         <button>6</button>
-        <button>-</button>
         <button>清空</button>
         <button>7</button>
         <button>8</button>
         <button>9</button>
-        <button>*</button>
         <button className = 'ok'>ok</button>
         <button>.</button>
         <button className = 'zero'>0</button>
-        <button>÷</button>
       </div>
     </Wrapper>
   );
